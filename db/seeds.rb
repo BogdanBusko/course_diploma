401dae61f388f0f33878f897db556b4d44ee78a2
BogdanBusko <busko.bogdan@gmail.com>
2017-11-01 20:16:52 +0200

    add new seeds data and modified view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,18 @@
     description: Faker::Educator.university,
     start_date: Faker::Time.between(DateTime.now, DateTime.now + 100),
     confirmed: Faker::Boolean.boolean,
-    organizer: Faker::Educator.university
+    organizer: Faker::Educator.university,
+    contacts: [
+      Contact.new(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.cell_phone
+      ),
+      Contact.new(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        phone: Faker::PhoneNumber.cell_phone
+      )
+    ]
   )
 end
